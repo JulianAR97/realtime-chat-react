b3e42e7ed03c93301027edd9f190c9db427050e5
Julian <julianrosenthal@yahoo.com>
2021-09-24 17:17:16 -0700

    added SideNav component

---
+++ src/SideNav.jsx
@@ -0,0 +1,27 @@
+import { makeStyles } from '@mui/styles'
+import React from 'react'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+const SideNav = () => {
+
+  const classes = useStyles()
+  
+  return (
+    <div className={classes.container}>
+
+      <div className={classes.header}>
+
+      </div>
+
+      <div className={classes.body}>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default SideNav
