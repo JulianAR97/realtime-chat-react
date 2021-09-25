3d954a875ae9a75ef1e1f88b2856eb4f4c6144ec
Julian <julianrosenthal@yahoo.com>
2021-09-24 17:17:04 -0700

    added some child components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { Paper } from '@mui/material'
+
+const useStyles = makeStyles(theme => ({
+  App: {
+    display: "grid",
+    placeItems: "center",
+    height: '100vh',
+  },
+  Paper: {
+    display: "flex",
+    height: "90vh",
+    width: "90vh"
+  }
+}))
+
 
 const App = () => {
+  const classes = useStyles()
+  
   return (
-    <div className="App">
-      
+    <div className={classes.App}>
+      <Paper className={classes.Paper}>
+
+      </Paper>
     </div>
   )
 }
