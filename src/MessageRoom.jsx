10f0d58fe58fa7fd16b57b07f1e80f4e9edbe357
Julian <julianrosenthal@yahoo.com>
2021-09-24 17:22:10 -0700

    added MessageRoom component

---
+++ src/MessageRoom.jsx
@@ -0,0 +1,28 @@
+import { makeStyles } from '@mui/styles'
+import React from 'react'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+const MessageRoom = () => {
+  const classes = useStyles()
+  
+  return (
+    <div className={classes.container}>
+      <div className="header">
+
+      </div>
+      <div className="body">
+
+      </div>
+      <div className="footer">
+        
+      </div>
+    </div>
+
+  
+  )
+}
+
+export default MessageRoom
