de3da4390c3e1fa0f4dde0e054ab63bc81004eab
Julian <julianrosenthal@yahoo.com>
2021-09-27 11:50:54 -0700

    added styles

--- src/MessageRoom.jsx
+++ src/MessageRoom.jsx
@@ -1,7 +1,49 @@
+import { Box, IconButton } from '@mui/material'
 import { makeStyles } from '@mui/styles'
+import { Send as SendIcon } from '@mui/icons-material'
 import React from 'react'
 
 const useStyles = makeStyles(theme => ({
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 0.7,
+  },
+  body: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    padding: '30px',
+    overflow: 'scroll',
+  },
+  header: {
+    padding: '20px',
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '1px solid lightgrey'
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '62px',
+    borderTop: '1px solid lightgrey',
+    backgroundColor: 'lightgrey',
+    padding: '10px'
+  },
+  messageForm: {
+    display: 'flex',
+    flex: 1,
+  },
+  messageInput: {
+    flex: 1,
+    borderRadius: '30px',
+    padding: '10px',
+    border: 'none',
+    outline: 'none'
+  },
+  messageSendButton: {
+    display: 'none'
+  }
 
 }))
 
@@ -9,19 +51,26 @@ const MessageRoom = () => {
   const classes = useStyles()
   
   return (
-    <div className={classes.container}>
-      <div className="header">
+    <Box className={classes.box}>
+      <div className={classes.header}>
+        I am MR
+      </div>
+      <div className={classes.body}>
 
       </div>
-      <div className="body">
-
+      <div className={classes.footer}>
+        <form className={classes.messageForm}>
+          <input
+            className={classes.messageInput} 
+            placeholder="Enter message..." 
+            type="text"
+          />
+          <IconButton>
+            <SendIcon />
+          </IconButton>
+        </form>
       </div>
-      <div className="footer">
-        
-      </div>
-    </div>
-
-  
+    </Box>
   )
 }
 
