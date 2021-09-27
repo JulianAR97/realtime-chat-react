d638468aadb09be51c6ee50c9a8b561be49b7cfd
Julian <julianrosenthal@yahoo.com>
2021-09-27 11:28:08 -0700

    added overflow styling

--- src/SideNav.jsx
+++ src/SideNav.jsx
@@ -1,8 +1,24 @@
+import { Container } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import React from 'react'
+import MessageGroup from './MessageGroup'
 
 const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 0.3,
+    borderRight: '1px solid black',
+    padding: '20px',
+    overflow: 'scroll'
+  },
+  header: {
 
+  },
+  body: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
 }))
 
 const SideNav = () => {
@@ -10,17 +26,19 @@ const SideNav = () => {
   const classes = useStyles()
   
   return (
-    <div className={classes.container}>
+    <Container className={classes.container}>
 
       <div className={classes.header}>
 
       </div>
 
       <div className={classes.body}>
-
+        <MessageGroup />
+        <MessageGroup />
+        <MessageGroup />
       </div>
       
-    </div>
+    </Container>
   )
 }
 
