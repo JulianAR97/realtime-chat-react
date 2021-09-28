0e40a792f2d4ffe3905a1de667852c574f905457
Julian <julianrosenthal@yahoo.com>
2021-09-27 17:15:28 -0700

    app subscribes to group snapshot from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Pusher from 'pusher-js'
 import { makeStyles } from '@mui/styles'
 import { Paper } from '@mui/material'
 import SideNav from './SideNav'
 import MessageRoom from './MessageRoom'
+import db from './firebase.js'
+import { onSnapshot, collection } from "firebase/firestore"
 
 const useStyles = makeStyles(theme => ({
   App: {
@@ -20,12 +23,41 @@ const useStyles = makeStyles(theme => ({
 
 const App = () => {
   const classes = useStyles()
+  const [groups, setGroups] = useState([])
+  const [messages, setMessages] = useState([])
+  const [selectedGroup, setSelectedGroup] = useState("")
   
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "groups"), (snapshot) => {
+      setGroups(snapshot.docs.map(doc => {
+        let { name, messages } = doc.data()
+        return ({
+          id: doc.id,
+          name,
+          messages
+        })
+      }
+      ))
+    });
+
+    return unsub
+    
+  }, [])
+
+  const handleGroupSelect = (groupID) => {
+    setSelectedGroup(groupID)
+    setMessages(() => {
+      let group = groups.find(g => g.id === groupID)
+      return group.messages
+    })
+  }
+  
+
   return (
     <div className={classes.App}>
       <Paper className={classes.Paper}>
-        <SideNav />
-        <MessageRoom />
+        <SideNav groups={groups} handleGroupSelect={handleGroupSelect} selectedGroup={selectedGroup}/>
+        <MessageRoom messages={messages}/>
       </Paper>
     </div>
   )
