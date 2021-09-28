1dc649778e0289298c451e50a5f87ac947873d2d
Julian <julianrosenthal@yahoo.com>
2021-09-28 13:11:02 -0700

    creates new message in firestore when submitted

--- src/components/messages/MessageForm.jsx
+++ src/components/messages/MessageForm.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { IconButton } from '@mui/material'
 import { Send as SendIcon } from '@mui/icons-material'
 import { makeStyles } from '@mui/styles'
+import { doc, updateDoc, Timestamp, arrayUnion } from 'firebase/firestore'
+import db from 'firebase.js'
+import { useAuth } from 'contexts/AuthContext'
+import { connect } from 'react-redux'
 
 const useStyles = makeStyles(theme => ({
   form: {
@@ -21,15 +25,48 @@ const MessageForm = (props) => {
   const classes = useStyles()
   
   const [input, setInput] = useState("")
+  const [loading, setLoading] = useState(true)
+ 
+  const { currentUser } = useAuth()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setLoading(true)
+    const username = currentUser.displayName
+    const uid = currentUser.uid  
+    
+    const docRef = doc(db, 'groups', props.selectedGroup.id)
+    
+    const newMessage = { 
+      username, 
+      uid, 
+      content: input, 
+      timestamp: Timestamp.fromDate(new Date()) 
+    }
+    
+    try {
+      updateDoc(docRef, {
+        messages: arrayUnion(newMessage)
+      })
+    } catch (err) {
+      alert(err.message)
+    } 
     
     // send to firebase
-    
+    setLoading(false)
     setInput("")
   }
 
+  const handleInputChange = (e) => {
+    if (e.target.value) {
+      setLoading(false)
+    } else {
+      setLoading(true)
+    }
+    setInput(e.target.value)
+  }
+
+  
   return (
     <form className={classes.form}>
       <input
@@ -37,9 +74,9 @@ const MessageForm = (props) => {
         placeholder={props.placeholder}
         type="text"
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={handleInputChange}
       />
-      <IconButton type="submit" onClick={handleSubmit}>
+      <IconButton type="submit" onClick={handleSubmit} disabled={loading}>
         <SendIcon />
       </IconButton>
     </form>
@@ -50,4 +87,8 @@ MessageForm.defaultProps = {
   placeholder: "Enter message..."
 }
 
-export default MessageForm
+const mapStateToProps = (state) => ({
+  selectedGroup: state.selectedGroup
+})
+
+export default connect(mapStateToProps, null)(MessageForm)
