c96cff77bc70ef84bdd59aa0ae3a9bad796831df
Julian <julianrosenthal@yahoo.com>
2021-09-30 11:26:17 -0700

    when user profile updates, update redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react'
 import { makeStyles } from '@mui/styles'
 import { Paper } from '@mui/material'
-import SideNav from 'components/nav/SideNav'
-import MessageRoom from 'components/messages/MessageRoom'
 import db from 'firebase.js'
-import { onSnapshot, collection } from "firebase/firestore"
+import { onSnapshot, collection , doc} from "firebase/firestore"
 import Login from 'components/auth/Login'
 import { useAuth } from 'contexts/AuthContext'
 import { setGroups } from 'actions/groupActions'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Chat from 'components/Chat'
+import { getProfile, setProfile } from 'actions/profileActions'
+
 
 const useStyles = makeStyles(theme => ({
   App: {
@@ -29,6 +31,7 @@ const App = (props) => {
   const { currentUser } = useAuth()
 
   const sg = props.setGroups
+  const sp = props.setProfile
   
   // setGroups when component loads or currentUser changes
   useEffect(() => {
@@ -45,23 +48,33 @@ const App = (props) => {
     return unsubscribe
   }, [sg, currentUser])
 
-  
+  // When currentUser changes, getProfile, and then setProfile in redux
+  useEffect(() => {
+    if (currentUser) {
+      getProfile(currentUser)
+        .then(profile => sp(profile))
+        .then(() => 
+          onSnapshot(doc(db, 'profiles', currentUser.uid), doc => {
+            const profile = doc.data()
+            sp(profile)
+          }))
+    }
+
+  }, [sp, currentUser])
+
 
   return (
 
     <div className={classes.App}>
       <Paper className={classes.Paper}>
-        {
-          currentUser ? (
-            <>
-              <SideNav />
-              <MessageRoom />
-            </>
-          )
-          : (
-            <Login />
-          )
-        }
+        <Router>
+          <Switch>
+            <Route path="/chat" component={Chat} />
+            <Route path="/login" component={Login} />
+
+            <Redirect from='*' to="/chat" />
+          </Switch>
+        </Router>
       </Paper>
     </div>
   
@@ -72,4 +85,4 @@ const mapStateToProps = state => ({
   groups: state.groups
 })
 
-export default connect(mapStateToProps, {setGroups})(App)
+export default connect(mapStateToProps, { setGroups, setProfile })(App)
