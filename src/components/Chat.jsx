964a7438bb418b80a24355d1ac06da7df882c12e
Julian <julianrosenthal@yahoo.com>
2021-09-30 10:07:59 -0700

    added Chat container component

---
+++ src/components/Chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MessageRoom from 'components/messages/MessageRoom'
+import SideNav from 'components/nav/SideNav'
+
+const Chat = () => {
+  return (
+    <>
+      <SideNav />
+      <MessageRoom />
+    </>
+  )
+}
+
+export default Chat
