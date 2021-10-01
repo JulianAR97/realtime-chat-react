e04b962c03d6500c829b718849fbf2671669d9be
Julian <julianrosenthal@yahoo.com>
2021-10-01 11:45:22 -0700

    NavSearch searches groups and then sets the matching groups in parent

--- src/components/nav/NavSearch.jsx
+++ src/components/nav/NavSearch.jsx
@@ -1,5 +1,5 @@
-import { makeStyles } from '@mui/styles'
 import React from 'react'
+import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -14,18 +14,64 @@ const useStyles = makeStyles(theme => ({
     border: 'none',
     outline: 'none',
     margin: '0px 10px',
-    width: '100%'
+    width: '100%',
+    fontSize: '16px'
+  },
+  filteredSearch: {
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    zIndex: 99999
+  },
+  match: {
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    zIndex: 99999
   }
 }))
 
-const NavSearch = () => {
-  
+const NavSearch = (props) => {
   const classes = useStyles()
+  
+  
+  const filterGroups = (text) => {
+    let matches = props.groups.filter(group => {
+      const regex = new RegExp(`${text}`, 'gi');
+      return group.name.match(regex)
+    })
+
+    // set matches from search in parent component
+    return matches
+  }
+
+  const handleChange = (e) => {
+    // unless search bar is empty, execute search
+    let matches = []
+    if (e.target.value) {
+      matches = filterGroups(e.target.value)
+    }
+
+    props.setGroupMatches(matches)
+  }
+
   return (
-    <div className={classes.container}>
-      <input className={classes.input} type="text" placeholder="Search chats" />
-    </div>
+    <>
+
+      <div className={classes.container}>
+        <input 
+          className={classes.input} 
+          onChange={handleChange}
+          type="text" 
+          placeholder="Search chats" 
+        />
+
+      </div>
+      
+    </>
   )
 }
+
+NavSearch.defaultProps = {
+  groups: [],
+  userGroups: []
+}
+
 
 export default NavSearch
