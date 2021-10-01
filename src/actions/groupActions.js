6131194623e7cbbcaf630cd7e408a9cf0461c195
Julian <julianrosenthal@yahoo.com>
2021-10-01 16:52:41 -0700

    added create group functionality

--- src/actions/groupActions.js
+++ src/actions/groupActions.js
@@ -1,4 +1,30 @@
+import db from 'firebase.js'
+import { collection, doc, setDoc, Timestamp } from 'firebase/firestore'
 
+// Firebase
+export const createGroup = async (name) => {
+  
+  try {
+    const newGroupRef = doc(collection(db, "groups"))
+
+    await setDoc(newGroupRef, {
+      name,
+      messages: [{
+        content: "Created new group",
+        timestamp: Timestamp.fromDate(new Date()),
+        uid: "123",
+        username: "Bot"
+      }]
+    })
+
+    return newGroupRef.id
+  } catch (err) {
+    alert(err)
+  }
+}
+
+
+// Redux
 export const setGroups = (groups) => {
   return dispatch => {
     dispatch({type: 'SET_GROUPS', payload: {groups}})
